6890d02b3f697b2b5ec08dad44705ebcf153ee7f
Kevinlyy <batmanthelegendly@gmail.com>
2018-12-14 01:02:40 -0500

    seg fix

--- control.c
+++ control.c
@@ -11,7 +11,6 @@
 #include <unistd.h>
 
 #define KEY 0xDEADBEEF
-// close shared memory por favor
 
 union semun {
   int              val;   // Value for SETVAL /
@@ -22,28 +21,32 @@ union semun {
 
 
 int main(int argc, char* argv[]) {
-  int key = 0xDEADBEEF;
+  if (argc < 2){
+    printf("No arguments \n");
+    exit(0);
+  }
 
-  printf("What would you like to do with the data? Create? Remove? or View? \n");
+  //printf("What would you like to do with the data? Create? Remove? or View? \n");
   char * args = argv[1];
 
   if(strcmp(args,"-c") == 0){
     int shmid;
     char * data;
 
-    shmid = shmget(key,200,0644 | IPC_CREAT);
+    shmid = shmget(KEY,1024,0644 | IPC_CREAT);
     data = shmat(shmid,(void *)0,0);
     if(data == (char*)(-1)){
       perror("shmat");
     }
 
+
     printf("Created shared memory \n");
 
     int semd;
-    semd= semget(key,1,IPC_CREAT | IPC_EXCL| 0777);
+    semd= semget(KEY,1,IPC_CREAT | IPC_EXCL| 0777);
     if(semd== -1){
-      printf("semaphore error\n");
-      semd = semget(key,1,0);
+      printf("semaphore already exists\n");
+      semd = semget(KEY,1,0);
       int v;
       v = semctl(semd,0,GETVAL,0);
       printf("semctl: %d \n",v);
@@ -61,13 +64,15 @@ int main(int argc, char* argv[]) {
     if(file < 0){
       printf("file error");
     }
+    printf("Created file \n");
 
   }
 
-  if(strcmp(args,"-r") == 0){
+
+  else if(strcmp(args,"-r") == 0){
     int shmid;
     shmid = shmget(0,200,0644 | IPC_CREAT);
-    int semd = semget(key,1,0);
+    int semd = semget(KEY,1,0);
 
     printf("waiting, semaphore unavailable at the moment\n");
     while(semctl(semd,0,GETVAL)==0);
@@ -77,8 +82,8 @@ int main(int argc, char* argv[]) {
     shmctl(semd,0,IPC_RMID);
 
     int file = open("file.txt", O_RDONLY);
-    char readin[10000000];
-    if(read(file,readin,10000000) < 0){
+    char readin[1000000];
+    if(read(file,readin,1000000) < 0){
       printf("error viewing \n");
       return 0;
     }
@@ -90,10 +95,10 @@ int main(int argc, char* argv[]) {
     shmctl(shmid, IPC_RMID, NULL);
     remove("file.txt");
 
-
   }
 
-  if(strcmp(args,"-v") == 0){
+
+  else if(strcmp(args,"-v") == 0){
     int file = open("file.txt", O_RDONLY);
 
     char readin[1000000];
@@ -106,9 +111,11 @@ int main(int argc, char* argv[]) {
     }
 
   }
+
   else{
-    printf("no argument \n");
+    printf("Wrong format\n");
   }
+
 
 
   return 0;
