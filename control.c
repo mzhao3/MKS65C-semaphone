42e92fee65a2eeaa017ff49b44792c905ae61f21
Kevinlyy <batmanthelegendly@gmail.com>
2018-12-13 22:33:35 -0500

    remove?

--- control.c
+++ control.c
@@ -10,16 +10,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-//#define KEY 0xDEADBEEF
+#define KEY 0xDEADBEEF
 // close shared memory por favor
-/**
+
 union semun {
-  int              val;    / Value for SETVAL /
-  struct semid_ds *buf;    / Buffer for IPC_STAT, IPC_SET /
-  unsigned short  *array;  / Array for GETALL, SETALL /
-  struct seminfo  *__buf;  / Buffer for IPC_INFO /
+  int              val;   // Value for SETVAL /
+  struct semid_ds *buf;   // Buffer for IPC_STAT, IPC_SET /
+  unsigned short  *array; // Array for GETALL, SETALL /
+  struct seminfo  *__buf; // Buffer for IPC_INFO /
 };
-**/
+
+
 int main(int argc, char* argv[]) {
   int key = 0xDEADBEEF;
 
@@ -50,7 +51,7 @@ int main(int argc, char* argv[]) {
 
     else {
       union semun us;
-      us.val = 3;
+      us.val = 1;
       int r;
       r =semctl(semd, 0, SETVAL, us);
       printf("Created semaphore: %d \n",r);
@@ -64,6 +65,30 @@ int main(int argc, char* argv[]) {
   }
 
   if(strcmp(args,"-r") == 0){
+    int shmid;
+    shmid = shmget(0,200,0644 | IPC_CREAT);
+    int semd = semget(key,1,0);
+
+    printf("waiting, semaphore unavailable at the moment\n");
+    while(semctl(semd,0,GETVAL)==0);
+
+    printf("Removing \n");
+
+    shmctl(semd,0,IPC_RMID);
+
+    int file = open("file.txt", O_RDONLY);
+    char readin[10000000];
+    if(read(file,readin,10000000) < 0){
+      printf("error viewing \n");
+      return 0;
+    }
+    else{
+      printf("Story time! \n%s \n",readin);
+    }
+
+    //remove
+    shmctl(shmid, IPC_RMID, NULL);
+    remove("file.txt");
 
 
   }
@@ -71,8 +96,8 @@ int main(int argc, char* argv[]) {
   if(strcmp(args,"-v") == 0){
     int file = open("file.txt", O_RDONLY);
 
-    char readin[10000000];
-    if(read(file,readin,10000000) < 0){
+    char readin[1000000];
+    if(read(file,readin,1000000) < 0){
       printf("error viewing \n");
       return 0;
     }
