667eb4cc4884e248d7d45cfdfac4a7b819961ba5
Maggie <mzhao3@stuy.edu>
2018-12-14 01:07:11 -0500

    main program working

--- control.c
+++ control.c
@@ -12,14 +12,14 @@
 
 #define KEY 0xDEADBEEF
 // close shared memory por favor
-
+/**
 union semun {
   int              val;   // Value for SETVAL /
   struct semid_ds *buf;   // Buffer for IPC_STAT, IPC_SET /
   unsigned short  *array; // Array for GETALL, SETALL /
   struct seminfo  *__buf; // Buffer for IPC_INFO /
 };
-
+**/
 
 int main(int argc, char* argv[]) {
   int key = 0xDEADBEEF;
--- makefile
+++ makefile
@@ -1,5 +1,12 @@
-all:
+all: sema.c control.c
+	gcc -g sema.c
+	gcc control.c -o control.out
 
+setup:
+	./control.out $(args)
 
 run:
 	./a.out
+
+clean:
+	rm -rf *.o *.out *.txt
